8ff89fca85ac80663cdfdc85e236c77401ec5540
kensho <112770791+kenshoHyo@users.noreply.github.com>
2023-01-09 22:59:34 +0900

    Add files via upload

---
+++ project/springmvcmodel/src/main/java/com/example/springmvcmodel/SpringmvcmodelApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcmodelApplication.class, args);
+	}
+
+}
---
+++ project/springmvcmodel/src/test/java/com/example/springmvcmodel/SpringmvcmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
