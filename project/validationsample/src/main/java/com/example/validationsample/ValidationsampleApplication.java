5f838ccc9c1507e5357036eae3d0fae92581726c
kensho <112770791+kenshoHyo@users.noreply.github.com>
2023-01-09 23:01:10 +0900

    Add files via upload

---
+++ project/validationsample/src/main/java/com/example/validationsample/ValidationsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.validationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationsampleApplication.class, args);
+	}
+
+}
---
+++ project/validationsample/src/test/java/com/example/validationsample/ValidationsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validationsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
