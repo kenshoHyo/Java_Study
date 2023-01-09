6e80b4d972b343c284b9900168b55832f6cf34e1
kensho <112770791+kenshoHyo@users.noreply.github.com>
2023-01-09 22:55:38 +0900

    Add files via upload

---
+++ project/pathvaliablesample/src/main/java/com/example/pathvaliablesample/PathvaliablesampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.pathvaliablesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathvaliablesampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathvaliablesampleApplication.class, args);
+	}
+
+}
---
+++ project/pathvaliablesample/src/test/java/com/example/pathvaliablesample/PathvaliablesampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pathvaliablesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathvaliablesampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
